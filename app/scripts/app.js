a46d631263a3ed6bbae89d6f204f837e33440138
Brian Jones <brian@brian-VirtualBox.(none)>
2013-04-08 20:58:11 +0100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('firstyoApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/users', {
+        templateUrl: 'views/users.html',
+        controller: 'UsersCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/users.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('firstyoApp')
+  .controller('UsersCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
